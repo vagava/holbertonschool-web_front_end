f6fd6171fc716197a21c48f2567d83dc39c4e078
Vanessa <vagava.123@gmail.com>
2022-09-27 00:04:46 -0500

    add 2-function_me.js

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,28 @@
+/**
+  * @description - Task 2
+  * @function welcomeMessage - Show Alert
+  * @param {String} fullname - Full Name
+  */
+
+function welcomeMessage (fullName) {
+  alert('Welcome ' + fullName);
+}
+
+/**
+  * guillaume - Show alert for name Guillaume
+  */
+guillaume = function () {
+  welcomeMessage('Guillaume');
+};
+/**
+  * alex - Show alert for name Alex
+  */
+alex = function () {
+  welcomeMessage('Alex');
+};
+/**
+  * fred - Show alert for name Fred
+  */
+fred = function () {
+  welcomeMessage('Fred');
+};

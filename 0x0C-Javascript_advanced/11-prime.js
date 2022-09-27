540c543dd13268b3b4b1500cd3d11f6613158f47
Vanessa <vagava.123@gmail.com>
2022-09-26 23:34:38 -0500

    add 11-prime.js

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,59 @@
+/**
+ * @description - Task 11
+ * @function countPrimeNumbers - Count number of primes from 2 to 100
+ * @returns {Number} - Primes count
+ */
+
+function countPrimeNumbers () {
+  /**
+     * @var {Boolean} isPrime - True if number is prime or False
+     * @var {Number} countPrimes - counter for primes
+     * @var {Number} i - iterator
+     * @var {Number} end - End limiter
+     * @var {Number} mod - mod
+     */
+
+  let isPrime;
+  let countPrimes = 0;
+
+  for (let i = 2; i <= 100; i++) {
+    isPrime = true;
+    const end = Math.round(Math.sqrt(i));
+
+    for (let mod = 2; mod <= end; mod++) {
+      if (i % mod == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      countPrimes = countPrimes + 1;
+    }
+  }
+
+  console.log(countPrimes);
+  return countPrimes;
+}
+
+/**
+ * @var {Number} startCountPrimeNumbers - Define start
+ * @var {Number} endCountPrimeNumbers - Define end
+ * @var {Number} j - iterator
+ */
+
+const startCountPrimeNumbers = window.performance.now();
+
+for (let j = 1; j < 101; j++) {
+  countPrimeNumbers();
+}
+
+const endCountPrimeNumbers = window.performance.now();
+
+/**
+ * Changing stack order with setTimeout
+ */
+
+setTimeout(function () {
+  console.log('Execution time of calculating prime numbers 100 times was ' + (endCountPrimeNumbers - startCountPrimeNumbers) + ' milliseconds.');
+}, 0);
